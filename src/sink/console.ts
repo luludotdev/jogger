360cd198bbe33b7e79826bf19b62cbba7865e2f0
Jack Baron <jackmbaron@gmail.com>
2021-01-19 03:40:48 +0000

    colourize console json

--- src/sink/console.ts
+++ src/sink/console.ts
@@ -1,3 +1,4 @@
+import colorize from 'json-colorizer'
 import type { ISink } from '.'
 
 export const consoleSink: (debug?: boolean) => Readonly<ISink> = (
@@ -5,19 +6,24 @@ export const consoleSink: (debug?: boolean) => Readonly<ISink> = (
 ) =>
   Object.freeze({
     out: log => {
-      process.stdout.write(log)
+      const out = process.stdout.isTTY ? colorize(log) : log
+
+      process.stdout.write(out)
       process.stdout.write('\n')
     },
 
     err: log => {
-      process.stderr.write(log)
+      const out = process.stderr.isTTY ? colorize(log) : log
+
+      process.stderr.write(out)
       process.stderr.write('\n')
     },
 
     debug: log => {
       if (debug === false) return
+      const out = process.stdout.isTTY ? colorize(log) : log
 
-      process.stdout.write(log)
+      process.stdout.write(out)
       process.stdout.write('\n')
     },
   })
