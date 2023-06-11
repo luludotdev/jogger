b80e28cba26786d69fc9c5b2f14f23d073012aa1
Jack Baron <jackmbaron@gmail.com>
2023-06-11 06:49:24 +0100

    rewrite

--- src/sink/index.ts
+++ src/sink/index.ts
@@ -1,7 +1,5 @@
-export {
-  createConsoleSink,
-  type Options as ConsoleSinkOptions,
-} from './console.js'
+export { createConsoleSink } from './console.js'
+export type { Options as ConsoleSinkOptions } from './console.js'
 export { createFileSink, type Options as FileSinkOptions } from './file.js'
 export { isSink } from './sink.js'
-export { type Sink } from './sink.js'
+export type { Sink } from './sink.js'
